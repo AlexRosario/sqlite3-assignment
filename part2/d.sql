2335b786586d24a5b49d79274b02794a69762c29
alexander rosario <alexanderrosario@alexanders-mbp.mynetworksettings.com>
2024-09-25 16:56:34 -0400

    finished assignment

--- part2/d.sql
+++ part2/d.sql
@@ -1,4 +1,3 @@
 -- Below is a dummy query, if we leave it empty then the SQLITE package will throw a non-readable error
 -- Change the query to pass the test :)
-select *
-from dogs;
+select Users.first_name, Users.last_name from Users join Favorites on Users.id = Favorites.user_id join Dogs on Dogs.id = Favorites.dog_id where Dogs.name = 'Zoey';
